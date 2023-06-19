0c09ece941e32169aac815afee3f5d5bde70237f
hillebrand <hillebrand@eleven.nl>
2023-06-19 12:32:27 +0200

    [OPSPORTAL-422] Ophalen van zabbix grafieken werken niet meer na update zabbix server

--- src/main/java/io/github/elevennl/zabbix/api/DefaultZabbixApi.java
+++ src/main/java/io/github/elevennl/zabbix/api/DefaultZabbixApi.java
@@ -1,162 +1,162 @@
 package io.github.elevennl.zabbix.api;
 
-import java.io.*;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.nio.charset.StandardCharsets;
-
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.DefaultHttpClient;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
+
 
 public class DefaultZabbixApi implements ZabbixApi {
-	private static final Logger logger = LoggerFactory.getLogger(DefaultZabbixApi.class);
+    private static final Logger logger = LoggerFactory.getLogger(DefaultZabbixApi.class);
+    private final URI uri;
+    ObjectMapper mapper = new ObjectMapper();
+    private HttpClient httpClient;
+    private String auth;
 
-	private HttpClient httpClient;
+    public DefaultZabbixApi(String url) {
+        try {
+            uri = new URI(url.trim());
+        } catch (URISyntaxException e) {
+            throw new RuntimeException("url invalid", e);
+        }
+    }
 
-	private URI uri;
+    public DefaultZabbixApi(URI uri) {
+        this.uri = uri;
+    }
 
-	private String auth;
+    public DefaultZabbixApi(String url, HttpClient httpClient) {
+        this(url);
+        this.httpClient = httpClient;
+    }
 
-	ObjectMapper mapper = new ObjectMapper();
+    public DefaultZabbixApi(URI uri, HttpClient httpClient) {
+        this(uri);
+        this.httpClient = httpClient;
+    }
 
-	public DefaultZabbixApi(String url) {
-		try {
-			uri = new URI(url.trim());
-		} catch (URISyntaxException e) {
-			throw new RuntimeException("url invalid", e);
-		}
-	}
+    @Override
+    public void init() {
+        if (httpClient == null) {
+            httpClient = HttpClientBuilder.create().build();
+        }
+    }
 
-	public DefaultZabbixApi(URI uri) {
-		this.uri = uri;
-	}
+    @Override
+    public void destroy() {
+        if (httpClient != null) {
+            httpClient = null;
+        }
+    }
 
-	public DefaultZabbixApi(String url, HttpClient httpClient) {
-		this(url);
-		this.httpClient = httpClient;
-	}
+    @Override
+    public boolean login(String user, String password) {
+        this.auth = null;
+        Request request = RequestBuilder.newBuilder().paramEntry("user", user)
+                .paramEntry("password", password).method("user.login").build();
+        JsonNode response = call(request, false);
+        String auth = response.path("result").textValue();
+        if (auth != null && !auth.isEmpty()) {
+            this.auth = auth;
+            return true;
+        }
+        return false;
+    }
 
-	public DefaultZabbixApi(URI uri, HttpClient httpClient) {
-		this(uri);
-		this.httpClient = httpClient;
-	}
+    @Override
+    public String apiVersion() {
+        Request request = RequestBuilder.newBuilder().method("apiinfo.version")
+                .build();
+        JsonNode response = call(request, false);
+        return response.path("result").textValue();
+    }
 
-	@Override
-	public void init() {
-		if (httpClient == null) {
-			httpClient = new DefaultHttpClient();
-		}
-	}
+    public boolean hostExists(String name) {
+        Request request = RequestBuilder.newBuilder().method("host.exists")
+                .paramEntry("name", name).build();
+        JsonNode response = call(request);
+        return response.path("result").booleanValue();
+    }
 
-	@Override
-	public void destroy() {
-		if (httpClient != null) {
-			httpClient = null;
-		}
-	}
+    public String hostCreate(String host, String groupId) {
+        ArrayNode groups = mapper.createArrayNode();
+        ObjectNode group = mapper.createObjectNode();
 
-	@Override
-	public boolean login(String user, String password) {
-		this.auth=null;
-		Request request = RequestBuilder.newBuilder().paramEntry("user", user)
-				.paramEntry("password", password).method("user.login").build();
-		JsonNode response = call(request, false);
-		String auth = response.path("result").textValue();
-		if (auth != null && !auth.isEmpty()) {
-			this.auth = auth;
-			return true;
-		}
-		return false;
-	}
+        group.put("groupid", groupId);
+        groups.add(group);
+        Request request = RequestBuilder.newBuilder().method("host.create")
+                .paramEntry("host", host).paramEntry("groups", groups).build();
+        JsonNode response = call(request);
+        return response.path("result").path("hostids").path(0).textValue();
+    }
 
-	@Override
-	public String apiVersion() {
-		Request request = RequestBuilder.newBuilder().method("apiinfo.version")
-				.build();
-		JsonNode response = call(request, false);
-		return response.path("result").textValue();
-	}
+    public boolean hostgroupExists(String name) {
+        Request request = RequestBuilder.newBuilder()
+                .method("hostgroup.exists").paramEntry("name", name).build();
+        JsonNode response = call(request);
+        return response.path("result").booleanValue();
+    }
 
-	public boolean hostExists(String name) {
-		Request request = RequestBuilder.newBuilder().method("host.exists")
-				.paramEntry("name", name).build();
-		JsonNode response = call(request);
-		return response.path("result").booleanValue();
-	}
+    /**
+     * @param name
+     * @return groupId
+     */
+    public String hostgroupCreate(String name) {
+        Request request = RequestBuilder.newBuilder()
+                .method("hostgroup.create").paramEntry("name", name).build();
+        JsonNode response = call(request);
+        return response.get("result").get("groupids").get(0).textValue();
+    }
 
-	public String hostCreate(String host, String groupId) {
-		ArrayNode groups = mapper.createArrayNode();
-		ObjectNode group = mapper.createObjectNode();
+    @Override
+    public JsonNode call(Request request) {
+        return call(request, true);
+    }
 
-		group.put("groupid", groupId);
-		groups.add(group);
-		Request request = RequestBuilder.newBuilder().method("host.create")
-				.paramEntry("host", host).paramEntry("groups", groups).build();
-		JsonNode response = call(request);
-		return response.path("result").path("hostids").path(0).textValue();
-	}
+    public JsonNode call(Request request, boolean needAuth) {
+        if (needAuth && request.getAuth() == null) {
+            request.setAuth(this.auth);
+        }
 
-	public boolean hostgroupExists(String name) {
-		Request request = RequestBuilder.newBuilder()
-				.method("hostgroup.exists").paramEntry("name", name).build();
-		JsonNode response = call(request);
-		return response.path("result").booleanValue();
-	}
+        byte[] byteArray = null;
+        try {
+            HttpPost httpRequest = new HttpPost(uri);
+            String requestStr = mapper.writeValueAsString(request);
+            httpRequest.setHeader("Content-type", "application/json-rpc");
+            if (request.getAuth() != null) {
+                httpRequest.setHeader("Authorization", "Bearer %s".formatted(request.getAuth()));
+            }
+            httpRequest.setEntity(new StringEntity(requestStr, ContentType.APPLICATION_JSON));
+            HttpResponse response = httpClient.execute(httpRequest);
+            HttpEntity entity = response.getEntity();
 
-	/**
-	 * 
-	 * @param name
-	 * @return groupId
-	 */
-	public String hostgroupCreate(String name) {
-		Request request = RequestBuilder.newBuilder()
-				.method("hostgroup.create").paramEntry("name", name).build();
-		JsonNode response = call(request);
-		return response.get("result").get("groupids").get(0).textValue();
-	}
+            byteArray = Utils.getBytesFromInputStream(entity.getContent());
+            InputStream inputStream = new ByteArrayInputStream(byteArray);
 
-	@Override
-	public JsonNode call(Request request){
-		return call( request, true );
-	}
-
-	public JsonNode call(Request request, boolean needAuth) {
-		if (needAuth && request.getAuth() == null) {
-			request.setAuth(this.auth);
-		}
-
-		byte[] byteArray = null;
-		try {
-			HttpPost httpRequest = new HttpPost(uri);
-			String requestStr = mapper.writeValueAsString(request);
-			httpRequest.addHeader("Content-Type", "application/json");
-			httpRequest.setEntity(new StringEntity(requestStr, ContentType.APPLICATION_JSON));
-			HttpResponse response = httpClient.execute(httpRequest);
-			HttpEntity entity = response.getEntity();
-
-			byteArray = Utils.getBytesFromInputStream(entity.getContent());
-			InputStream inputStream = new ByteArrayInputStream(byteArray);
-
-			return mapper.readTree(inputStream);
-		} catch (IOException e) {
-			if(byteArray == null ) {
-				throw new RuntimeException("DefaultZabbixApi call exception! empty response, ", e);
-			}else {
-				throw new RuntimeException("DefaultZabbixApi call exception! " + new String(byteArray, StandardCharsets.UTF_8) + ", ", e);
-			}
-		}
-	}
+            return mapper.readTree(inputStream);
+        } catch (IOException e) {
+            if (byteArray == null) {
+                throw new RuntimeException("DefaultZabbixApi call exception! empty response, ", e);
+            } else {
+                throw new RuntimeException("DefaultZabbixApi call exception! " + new String(byteArray, StandardCharsets.UTF_8) + ", ", e);
+            }
+        }
+    }
 
 }
